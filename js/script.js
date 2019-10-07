bb1ada5e18e2d2ac8326cb6f77f70428eb4f48c6
paulyo19 <catkillercleary@gmail.com>
2019-10-07 20:38:15 +0100

    adding an array object of quotes

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var quotes = [
+    {
+        quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+        source: '-Nelson Mandela',
+        citation: 'speech',
+        year: 1970
+    },
+    {
+        quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+        source: '-Nelson Mandela',
+        citation: 'speech',
+        year: 1970
+    },
+    {
+        quote: 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.',
+        source: '-Mark Twain',
+        citation: 'speech',
+        year: 1885
+    },
+    {
+        quote: 'You can do anything, but not everything.',
+        source: '-David Allen',
+        citation: 'speech',
+        year: 1965
+    },
+    {
+        quote: 'Great men are not born great, they grow great.',
+        source: '-Mario Puzo',
+        citation: 'Movie',
+        year: 1972
+    },
+    {
+        quote: "You, me, or nobody is gonna hit as hard as life. But it ain't about how hard you hit. It's about how hard you can get hit and keep moving forward. How much you can take and keep moving forward. That's how winning is done! ",
+        source: '-Rocky Balboa',
+        citation: 'Movie',
+        year: 1976
+    },
+];

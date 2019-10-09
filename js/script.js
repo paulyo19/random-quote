734b91e6e0272aa4f36928d7c6cc474e2dc52fab
paulyo19 <catkillercleary@gmail.com>
2019-10-09 22:05:54 +0100

    background color will change when user clicks on next quote

--- js/script.js
+++ js/script.js
@@ -81,6 +81,18 @@ function printQuote() {
 }
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", random_bg_color, false);
 
 
 
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+
+random_bg_color();
